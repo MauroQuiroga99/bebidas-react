65bcfeec99d962b43d4d32d5a3bfa7955f38c65c
Mauro Quiroga <148659366+MauroQuiroga99@users.noreply.github.com>
2024-08-20 23:06:51 -0500

    añadir img de fondo para el inicio

--- src/Componets/Header.tsx
+++ src/Componets/Header.tsx
@@ -10,7 +10,13 @@ const Header = () => {
   console.log(isHome);
 
   return (
-    <header className="bg-slate-800">
+    <header
+      className={
+        isHome
+          ? " bg-center bg-cover bg-[url('/public/bg.jpg')]"
+          : "bg-slate-800"
+      }
+    >
       <div className="mx-auto container px-5 py-16">
         <div className="flex justify-between items-center ">
           <div>
