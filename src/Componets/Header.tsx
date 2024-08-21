713f59ede2c99cba6cec3816fc1a4771de450d60
Mauro Quiroga <148659366+MauroQuiroga99@users.noreply.github.com>
2024-08-20 22:54:35 -0500

    crear formulario de busqueda

--- src/Componets/Header.tsx
+++ src/Componets/Header.tsx
@@ -1,8 +1,14 @@
+import { useMemo } from "react";
 import { NavLink, useLocation } from "react-router-dom";
 
 const Header = () => {
-  const pathname = useLocation();
+  const { pathname } = useLocation();
+
   console.log(pathname);
+
+  const isHome = useMemo(() => pathname === "/", [pathname]);
+  console.log(isHome);
+
   return (
     <header className="bg-slate-800">
       <div className="mx-auto container px-5 py-16">
@@ -33,6 +39,46 @@ const Header = () => {
             </NavLink>
           </nav>
         </div>
+        {isHome && (
+          <form className=" md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6">
+            <div className="space-y-4">
+              <label
+                className="block text-white uppercase font-extrabold text-lg "
+                htmlFor="ingredient"
+              >
+                Nombre o Ingredientes
+              </label>
+              <input
+                id="ingredient"
+                className="p-3 w-full rounded-lg focus:outline-none"
+                name="ingredient"
+                type="text"
+                placeholder="Nombre o Ingrediente. Ej. Vodka, Tequila, Café"
+              />
+            </div>
+            <div className="space-y-4">
+              <label
+                className="block text-white uppercase font-extrabold text-lg "
+                htmlFor="ingredient"
+              >
+                Categoría
+              </label>
+              <select
+                id="category"
+                className="p-3 w-full rounded-lg focus:outline-none"
+                name="ingredient"
+              >
+                <option value="">--Seleccione--</option>
+              </select>
+            </div>
+
+            <input
+              type="submit"
+              value="Buscar Recetas"
+              className=" p-2 cursor-pointer bg-orange-800 hover:bg-orange-900 text-white font-extrabold w-full rounded-lg uppercase"
+            />
+          </form>
+        )}
       </div>
     </header>
   );
