9428c4d4a1df59e91e439533114702624637f109
Mauro Quiroga <148659366+MauroQuiroga99@users.noreply.github.com>
2024-10-24 05:53:44 -0500

    agregar margin en los componentes padres de las cartas

--- src/views/IndexPage.tsx
+++ src/views/IndexPage.tsx
@@ -11,7 +11,7 @@ const IndexPage = () => {
     <>
       <h1 className="text-6xl font-extrabold">Recetas</h1>
       {hasDrinks ? (
-        <div className=" grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 my-10 gap-10 ">
+        <div className="  mx-9 grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 my-10 gap-10 ">
           {drinks.map((drink) => (
             <DrinkCard key={drink.idDrink} drink={drink} />
           ))}
